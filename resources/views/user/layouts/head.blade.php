300efc306a71ce7fab94f2653b2425060656e6e8
ousmanekedougou <yabaye075@gmail.com>
2021-04-21 15:56:17 +0000

    reglage

--- resources/views/user/layouts/head.blade.php
+++ resources/views/user/layouts/head.blade.php
@@ -29,8 +29,5 @@
 			<link rel="stylesheet" href="{{asset('user/css/main.css')}}">
 
 
-					<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css">
-
 			  @section('head')
   				@show
