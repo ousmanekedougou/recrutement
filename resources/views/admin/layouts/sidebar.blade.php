99199730da1c7ff3c436b17b61773f8a50f9bfa7
ousmanekedougou <yabaye075@gmail.com>
2021-06-30 23:06:34 +0000

    admin2

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -8,25 +8,20 @@
         </div>
         <div class="pull-left info">
           <p>{{Auth::guard('web')->user()->name}}</p>
-          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+          @if(Auth::user()->isAdmin == 1)
+          <a href="#"><i class="fa fa-circle text-success"></i> Admistrateur</a>
+          @else 
+          <a href="#"><i class="fa fa-circle text-success"></i> Utilisateur</a>
+          @endif
         </div>
       </div>
       <!-- search form -->
-      <form action="#" method="get" class="sidebar-form">
-        <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search...">
-          <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-        </div>
-      </form>
       <!-- /.search form -->
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
       
 
-        <li class="header">LABELS</li>
+        <li class="header">PARAMETRES</li>
         <li><a href="{{ route('filliere.index') }}"><i class="fa fa-file-text"></i> <span>Filliers</span></a></li>
         <li><a href="{{ route('etablissement.index') }}"><i class="fa fa-university"></i> <span>Etablissement</span></a></li>
         <li><a href="{{ route('home.index') }}"><i class="fa fa-graduation-cap"></i> <span>Etudiants</span></a></li>
