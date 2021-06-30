5b1d5531c42e0f65d4438aff57cd98fef348a07d
ousmanekedougou <yabaye075@gmail.com>
2021-06-30 13:38:15 +0000

    reglage

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,8 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('status');
+            $table->integer('image');
+            $table->integer('isAdmin');
             $table->rememberToken();
             $table->timestamps();
         });
