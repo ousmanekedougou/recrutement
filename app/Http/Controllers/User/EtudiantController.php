7331e0267ea2d7de64ca4c937feb1ce37feb97ff
ousmanekedougou <yabaye075@gmail.com>
2021-07-04 02:35:56 +0000

    phone

--- app/Http/Controllers/User/EtudiantController.php
+++ app/Http/Controllers/User/EtudiantController.php
@@ -14,7 +14,7 @@ class EtudiantController extends Controller
             'genre' => 'required',
             'name' => 'required|string',
             'email' => 'required|email|unique:etudiants',
-            'phone' => 'required|unique:etudiants|numeric|regex:/^([0-9\s\-\+\(\)]*)$/|between:9,13',
+            'phone' => 'required|unique:etudiants|numeric|regex:/^([0-9\s\-\+\(\)]*)$/',
             'date' => 'required|string',
             'lieu' => 'required|string',
             'niveau' => 'required|string',
